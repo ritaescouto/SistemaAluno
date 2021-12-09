56e3a4458a0ac8f9aaf5135653e98d04b10b7b0c
ritaescouto <94724220+ritaescouto@users.noreply.github.com>
2021-12-09 20:45:28 -0300

    Add files via upload

---
+++ Aluno.js
@@ -0,0 +1,35 @@
+let nomeDeAluno = ["Marcel","Rita","Afrânio"]
+for (let nome of nomeDeAluno){
+    console.log(`Esta pessoa se chama ${nome}`)
+}
+
+// let numeroDeAlunos = 10;
+// let contador = 0;
+
+
+// // Exemplo while
+
+// while (contador <= numeroDeAlunos){
+//   // console.log(contador)
+
+//    if (contador == 0){
+//        console.log (" O numero atual é zero")
+//    }else if(contador % 2 != 0) {
+//        console.log ("o numero " + contador + " é Impar")
+//    }else {
+//     console.log(`o numero $ {contador}  é Par`)
+//    }
+//    contador++
+// }
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//     console.log(contador)
+ 
+//     if (contador == 10){
+//         console.log (" O numero atual é zero")
+//     }else if(contador % 2 == 0) {
+//         console.log ("o numero " + contador + " é Par")
+//     }else {
+//      console.log('o numero $ {contador}  é impar')
+//     }
+//  }
